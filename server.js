d9791ad73917b57017ade67722ab3bdc6407c975
krisjack5 <krisjack29@gmail.comkrisjack5>
2017-04-25 22:45:54 -0400

    still working

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+
+const app = express();
+var PORT = process.env.PORT || 8080;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
