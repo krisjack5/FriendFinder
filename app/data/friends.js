2abaa93a8cd8ce64e46beb32e8cd27be0c42dfd6
krisjack5 <krisjack29@gmail.comkrisjack5>
2017-04-29 15:31:31 -0400

    ehhh

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,37 +1,18 @@
-// FRIENDS
-// =============================================================
+var = friendsData[{
+   "name":"Ahmed",
+   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+   "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+ }];
 
-var friendArray = [
-{
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-},
-{
-    name: "Two",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [
-        1,
-        5,
-        2,
-        2,
-        1,
-        5,
-        2,
-        1,
-        4,
-        5
-    ]
-}
-];
+ module.exports = friendsData;
--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var path = require("path");
 
-
-
 module.exports = function(app) {
 
  
